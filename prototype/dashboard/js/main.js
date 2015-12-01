b2f19e404c7f0e4a77c79bf5971716a4063d6ba7
Jochen Walther <waltherjochen@web.de>
2015-12-01 15:27:46 +0100

    navbar
    center aligned the apps
    
    Change-Id: I4e75d2f91150eb9024ab0db92b6395116607ba82

--- prototype/dashboard/js/main.js
+++ prototype/dashboard/js/main.js
@@ -11,3 +11,21 @@ $('#englishCourse').hover(function() {
 },function() {
     $('#englishIcon', this).stop().animate({"opacity": 1});
 });
+
+$('#musicCourse').hover(function() {
+    $('#musicIcon', this).stop().animate({"opacity": 0.8});
+},function() {
+    $('#musicIcon', this).stop().animate({"opacity": 1});
+});
+
+$('#religionCourse').hover(function() {
+    $('#religionIcon', this).stop().animate({"opacity": 0.8});
+},function() {
+    $('#religionIcon', this).stop().animate({"opacity": 1});
+});
+
+$('#artCourse').hover(function() {
+    $('#artIcon', this).stop().animate({"opacity": 0.8});
+},function() {
+    $('#artIcon', this).stop().animate({"opacity": 1});
+});
