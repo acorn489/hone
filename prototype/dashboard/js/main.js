6a62742c3df5c035a1787bed751a4c765a8f964b
Jochen Walther <waltherjochen@web.de>
2015-12-17 12:36:39 +0100

    asdfgh
    Change-Id: Ib4366e9ba7e85870bba3ae256773da7b2ad0826e

--- prototype/dashboard/js/main.js
+++ prototype/dashboard/js/main.js
@@ -29,3 +29,33 @@ $('#artCourse').hover(function() {
 },function() {
     $('#artIcon', this).stop().animate({"opacity": 1});
 });
+
+$(document).ready(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel({interval: false, pause: true});
+});
+
+
+//Fix Navbar when scroll
+$(document).ready(function()
+{
+    var navbar = $('.menu');
+
+    navbar.after('<div id="navbarContainer" style="height: ' + $('.menu').height() + 'px" class="hidden"></div>');
+    var container = $('#navbarContainer');
+
+
+    $(window).on('scroll', function()
+    {
+        if ($(window).scrollTop() > $('.menu').height())
+        {
+            navbar.addClass('navbar-fixed-top');
+            container.removeClass('hidden');
+        }
+        else
+        {
+            navbar.removeClass('navbar-fixed-top');
+            container.addClass('hidden');
+        }
+    });
+});
