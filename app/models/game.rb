f7b9b43527b1956eb9c6bd227d003ff203717b49
Jochen Walther <waltherjochen@web.de>
2016-04-04 09:45:47 +0200

    Redesigned new Game page
    Change-Id: I134834a32b374cc3101ea48cd4b4c3b04516848e

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,5 @@ class Game < ActiveRecord::Base
   has_many :game_skills
   has_many :skills, :through => :game_skills, :foreign_key => 'game_id'
 
-  validates :title, :description, :platform, :link, presence: true
+  validates :title, presence: true
 end
