a49e53ddd39be0249203f0365daa1c7b86ee4933
Jochen Walther <waltherjochen@web.de>
2016-04-07 10:21:57 +0200

    Write new game form data in db
    Change-Id: Ib3cdd9abf6095a87b9cf0959da254f93c34da93e

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,5 @@ class Game < ActiveRecord::Base
   has_many :game_skills
   has_many :skills, :through => :game_skills, :foreign_key => 'game_id'
 
-  validates :title, presence: true
+  validates :title, :android_link, :ios_link, :web_link, presence: true
 end
