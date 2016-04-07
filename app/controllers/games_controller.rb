fdab6b8d346b4e5059ce733a876889426332582c
Jochen Walther <waltherjochen@web.de>
2016-04-07 10:21:57 +0200

    Write new game form data in db
    Change-Id: Ib3cdd9abf6095a87b9cf0959da254f93c34da93e

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,26 +2,23 @@ class GamesController < ApplicationController
   before_action :authenticate_developer!
 
   def new
-    # @skill = Skill.friendly.find(params[:skill])
     @game = Game.new
   end
 
   def create
-    # @skill = Skill.friendly.find(params.require(:game).permit(:skill)[:skill])
     @game = Game.create(game_params)
+    skills.each do |item|
+      GameSkill.create(:game_id => @game.id, :skill_id => item)
+    end
     redirect_to :action => 'new'
-    # if @game.save
-    #   # GameSkill.create(game_id: @game.id, skill_id: @skill.id)
-    #   flash[:notice] = "Game Added"
-    #   # redirect_to @game.skills[0] # TODO: support adding a game with more than one skill
-    # else
-    #   flash[:notice] = "Please fill in all fields."
-    #   redirect_to :action => 'new', :skill => @skill.slug
-    # end
   end
 
   private
   def game_params
-    params.require(:game).permit(:title)
+    params.require(:game).permit(:title, :android_link, :ios_link, :web_link)
+  end
+
+  def skills
+    params[:skillBox]
   end
 end
