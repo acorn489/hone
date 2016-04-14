dbe2676838da7e4e1d7711d91727c4c57c7f651d
Dennis Latt <kontakt@latt.org>
2016-04-14 15:13:53 +0200

    tagging system for my languages
    - overwrites the default controller of devise, to create an entry in db
    in reference to the user_id
    
    Change-Id: Id0615b78564477185f268f874b9051b7509bbeea

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,27 @@
+class RegistrationsController < Devise::RegistrationsController
+  after_action :create_languages, only: :create
+
+  def create
+    super
+  end
+
+  def new
+    super
+  end
+
+  def languages
+    params[:user][:languages].drop(1)
+  end
+
+  def update
+    super
+  end
+
+  private
+
+  def create_languages
+    languages.each do |item|
+      UserLanguage.create(user_id: @user.id, language: item) if @user.persisted?
+    end
+  end
+end
