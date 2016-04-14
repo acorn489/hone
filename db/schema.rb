f5ca0e009016c4da768039e4625f585ba2555810
Christian Petrov <wwhhooaahh@gmail.com>
2016-04-15 01:55:24 +0200

    Improve game creation UX and introduce games list
    * Implement error handling.
    * Show success message if game creation was successful.
    * Update model to include developer id.
    * Create a list on the main page showing all games of the currently
    active developer and style the "Add ..." button.
    * Introduce "Redirect URI" to model and form. It is needed for returning to
    the game after app approval during OAuth handshake.
    * Move game creation logic to model. Fat controllers are bad.
    
    Known issues:
    * upon error skills multiple select content is not retained.
    * validation is not enabled for the skills select. It is tricky to check for
    validity as select2 adds an empty string element to the query parameter,
    so the array is never empty. We may need to validate on our own.
    
    Change-Id: I3ecbac0e49964dfc9be9d1388b15fd82a888d6e5

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414121803) do
+ActiveRecord::Schema.define(version: 20160414234249) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160414121803) do
     t.integer  "skill_id",     limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "developer_id", limit: 4
   end
 
   add_index "games", ["skill_id"], name: "index_games_on_skill_id", using: :btree
