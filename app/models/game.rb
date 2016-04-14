27a7ce2fd9c48e30a121aae7254abbb44912b6ad
Christian Petrov <wwhhooaahh@gmail.com>
2016-04-14 20:49:00 +0200

    Add complete_skill service
    This service is protected by the OAuth 2.0 Implicit strategy. OAuth 2.0 is
    provided by doorkeeper.
    
    * Rename students_controller to web_api_controller. It will house API consumed by
    the website itself through AJAX. Its resources are protected by session
    authentification.
    
    * game_api_controller will house API consumed by 3rd party games. Its
    resources are protected by OAuth.
    
    Change-Id: I20f40164f8b270af5abef0eb35ebccb7a56f40e2

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ActiveRecord::Base
   has_many :game_skills
+  has_many :oauth_applications, class_name: 'Doorkeeper::Application', as: :owner
   has_many :skills, :through => :game_skills, :foreign_key => 'game_id'
 
   validates :title, presence: true
