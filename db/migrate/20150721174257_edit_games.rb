29cd99e114e46d2fbb37f9fbd03da60a7581ccfd
Christian Petrov <wwhhooaahh@gmail.com>
2016-04-18 21:43:48 +0200

    Remove invalid migrations
    It looks like create_games migration was deleted at some point.
    This means that all the following migrations are invalid. Running
    migrations on MySQL failed gracefully, but SQLite3 doesn't like that.
    
    Change-Id: Ia8c1292930055a841af4cc4857496c1c49b32a80

--- db/migrate/20150721174257_edit_games.rb
+++
@@ -1,5 +0,0 @@
-class EditGames < ActiveRecord::Migration
-  def change
-    remove_column :games, :status
-  end
-end
--- db/migrate/20150728213551_removeskillsgamesassociation.rb
+++
@@ -1,5 +0,0 @@
-class Removeskillsgamesassociation < ActiveRecord::Migration
-  def change
-    remove_column :games, :skill_id
-  end
-end
