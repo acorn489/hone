f8c487b5c39e115ed052b212ffa994402e249b67
Christian Petrov <wwhhooaahh@gmail.com>
2016-04-20 01:59:12 +0200

    Add skill service documentation
    Change-Id: Iab5090e9dd2c5b11b3c3d793689e0cc6d251a22b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def title(text)
+    content_for :title, text
+  end
+
   %w(Student Developer Admin).each do |k|
     define_method "current_#{k.underscore}" do
       current_user if current_user.is_a?(k.constantize)
