409fd9fb1d85b35797427fa773d7f82aa1ef3c8e
Christian Petrov <wwhhooaahh@gmail.com>
2016-04-20 13:53:30 +0200

    Tidy up env vars and prepare for a config yml
    Project environment variables can now be declared in
    config/local_env.yml like that:
    
      MYSQL_SOCKET: /var/run/mysqld/mysqld.sock
      HONE2_0_DEVELOPMENT_DATABASE_USERNAME: root
      HONE2_0_DEVELOPMENT_DATABASE_PASSWORD: password
    
    Tidy up variable names:
      * don't use HONE2_0_DEVELOPMENT_... vars for all environments, but
    only for development.
      * don't hard-code MySQL socket but export it as a variable.
      * rename HONE2_0_DATABASE_PASSWORD -> HONE2_0_PRODUCTION_DATABASE_...
    as it was used for production DB.
    
    Change-Id: I16f8d3cb1ce94841b571fba2cb209a973406009b

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module Hone20
         resource '/api/*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
