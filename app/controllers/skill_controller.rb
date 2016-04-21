f983a7cdc88b5148b152b89d18dbf1193f1ffb5a
Christian Petrov <wwhhooaahh@gmail.com>
2016-04-21 22:41:18 +0200

    Require student session for /skill and /skills
    Skill and skills can only work with a student session.
    
    Fix #14.
    
    Change-Id: Ia621c07ef4c89ad1918ba1cee573252a2159c3c3

--- app/controllers/skill_controller.rb
+++ app/controllers/skill_controller.rb
@@ -1,7 +1,8 @@
 class SkillController < ApplicationController
-    def show
-      current_student = Student.find(session['warden.user.user.key'][0][0])
-      @skill = Skill.find(params[:id])
-      @games = @skill.games
-    end
+  before_action :authenticate_student!
+  def show
+    current_student = Student.find_by(id: session['warden.user.user.key'][0][0])
+    @skill = Skill.find_by(id: params[:id])
+    @games = @skill.games
+  end
 end
--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,4 +1,5 @@
 class SkillsController < ApplicationController
+  before_action :authenticate_student!
   def show
     @course = Course.find_by_slug(params[:course])
   end
